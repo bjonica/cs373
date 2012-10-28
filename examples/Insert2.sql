550f5ee13525642a94542a6dda4a930ed5d358dc
Glenn Downing <downing@cs.utexas.edu>
2012-10-28 17:12:47 -0500

    another commit

--- examples/Insert2.sql
+++ examples/Insert2.sql
@@ -1,5 +1,53 @@
 use downing_test;
 
+drop table if exists movies_actors;
+drop table if exists actors;
+drop table if exists movies_awards;
+drop table if exists awards;
+drop table if exists movies;
+drop table if exists directors;
+
+create table directors (
+        director_id   int  not null auto_increment,
+        director_name text not null,
+        primary key   (director_id))
+    engine = innodb;
+
+create table movies (
+        movie_id    int  not null auto_increment,
+        movie_name  text not null,
+        movie_year  int  not null,
+        director_id int  not null,
+        primary key (movie_id),
+        foreign key (director_id) references directors (director_id))
+    engine = innodb;
+
+create table awards (
+        award_id    int  not null auto_increment,
+        award_name  text not null,
+        primary key (award_id))
+    engine = innodb;
+
+create table movies_awards (
+        movie_id    int not null,
+        award_id    int not null,
+        foreign key (movie_id) references movies (movie_id),
+        foreign key (award_id) references awards (award_id))
+    engine = innodb;
+
+create table actors (
+        actor_id    int  not null auto_increment,
+        actor_name  text not null,
+        primary key (actor_id))
+    engine = innodb;
+
+create table movies_actors (
+        movie_id    int not null,
+        actor_id    int not null,
+        foreign key (movie_id) references movies (movie_id),
+        foreign key (actor_id) references actors (actor_id))
+    engine = innodb;
+
 insert into directors values (null, "Barry Levinson");
 insert into directors values (null, "Francis Ford Coppola");
 insert into directors values (null, "George Cukor");
@@ -152,5 +200,12 @@ insert into movies_actors values (16, 13);
 insert into movies_actors values (16, 32);
 insert into movies_actors values (17, 22);
 insert into movies_actors values (17, 31);
+
+select * from directors;
+select * from movies;
+select * from awards;
+select * from movies_awards;
+select * from actors;
+select * from movies_actors;
 
 exit
--- examples/List1.sql
+++
@@ -1,5 +0,0 @@
-use downing_test;
-
-select * from movies;
-
-exit
