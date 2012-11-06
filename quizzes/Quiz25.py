c0e443038f6f9ca239ebdc1edd537f79e99c6bd2
Glenn Downing <downing@cs.utexas.edu>
2012-11-05 22:06:33 -0600

    another commit

---
+++ quizzes/Quiz25.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+"""
+CS373: Quiz #25 (5 pts)
+"""
+
+""" ----------------------------------------------------------------------
+1. What is a "hallway usability test"?
+   [The Joel Test]
+   (1 pts)
+
+A hallway usability test is where you grab the next person that passes by
+in the hallway and force them to try to use the code you just wrote. If
+you do this to five people, you will learn 95% of what there is to learn
+about usability problems in your code.
+"""
+
+""" ----------------------------------------------------------------------
+2. The Joel Test consists of 11 other questions?
+   List any three.
+   [The Joel Test]
+   (3 pts)
+
+1.  Do you use source control?
+2.  Can you make a build in one step?
+3.  Do you make daily builds?
+4.  Do you have a bug database?
+5.  Do you fix bugs before writing new code?
+6.  Do you have an up-to-date schedule?
+7.  Do you have a spec?
+8.  Do programmers have quiet working conditions?
+9.  Do you use the best tools money can buy?
+10. Do you have testers?
+11. Do new candidates write code during their interview?
+"""
