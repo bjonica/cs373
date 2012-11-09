98fc025a97864e8737861f0fa3b93a3a0cd74bbc
Glenn Downing <downing@cs.utexas.edu>
2012-11-09 15:59:50 -0600

    another commit

---
+++ quizzes/Quiz27.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+"""
+CS373: Quiz #27 (5 pts)
+"""
+
+""" ----------------------------------------------------------------------
+1. What is the multiplicity between Rental and Customer?
+   [RF: Ch. 1, pg. 2]
+   (1 pt)
+
+many to one
+"""
+
+""" ----------------------------------------------------------------------
+2. What needs to be built as the first step in refactoring?
+   [RF: Ch. 1, pg. 7]
+   (1 pt)
+
+a set of tests
+"""
+
+""" ----------------------------------------------------------------------
+3. What is the termination condition for a Java iterator?
+   For a Python iterator?
+   (2 pts)
+
+hasNext() returns False
+a StopIteration exception
+"""
