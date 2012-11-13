de80afbb96b0429cf56e491d387867542747b59a
Glenn Downing <downing@cs.utexas.edu>
2012-11-12 18:22:56 -0600

    another commit

---
+++ quizzes/Quiz28.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+"""
+CS373: Quiz #28 (5 pts)
+"""
+
+""" ----------------------------------------------------------------------
+1. List any two of the four suggestions in Stroustrup's conclusion.
+   [What Should We Teach New Software Developers? Why?]
+   (2 pts)
+
+shrink academia/industry gap
+define a CS core with specializations and application areas
+license software and developers
+lifelong industry/academia involvement for technical experts
+"""
+
+""" ----------------------------------------------------------------------
+2. Which design pattern of the Gang of Four does the author use for the
+   last refactoring?
+   [http://www.refactoring.com/catalog/replaceTypeCodeWithStateStrategy.html]
+   [RF: Ch. 1, pg. 38]
+   (2 pts)
+
+State / Strategy
+"""
