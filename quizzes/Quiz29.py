96b9ceff4f60281d42b29a4560d070acb19df412
Glenn Downing <downing@cs.utexas.edu>
2012-11-15 10:53:43 -0600

    another commit

---
+++ quizzes/Quiz29.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+"""
+CS373: Quiz #29 (5 pts)
+"""
+
+""" ----------------------------------------------------------------------
+1. What is a published interface (a step beyond a public interface)?
+   [RF: Ch. 2, pg. 64]
+   (1 pt)
+
+an interface that is used by code that you don't control
+"""
+
+""" ----------------------------------------------------------------------
+2. What question should a good comment answer?
+   [RF: Ch. 3, pg. 88]
+   (1 pt)
+
+why?
+"""
+
+""" ----------------------------------------------------------------------
+3. Describe any two of the three problems in finding references to a
+   field, method, or class, with a compiler.
+   [RF: Ch. 5, pg. 105]
+   (2 pts)
+
+multiple declarations
+too slow
+reflection
+"""
