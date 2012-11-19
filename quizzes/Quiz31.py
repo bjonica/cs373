4bc21abfd2ab3b9d0127c051fab4ae5bf88a7228
Glenn Downing <downing@cs.utexas.edu>
2012-11-19 16:00:14 -0600

    another commit

---
+++ quizzes/Quiz31.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+"""
+CS373: Quiz #31 (5 pts)
+"""
+
+""" ----------------------------------------------------------------------
+1. The Self-Adaptive Process
+   The first part of self-adaptivity is regular reviews of the process.
+   Usually you do these with every iteration. At the end of each
+   iteration, have a short meeting and ask yourself the following
+   questions (culled from Norm Kerth)...
+   List any two of the four:
+   [The New Methodology]
+   (2 pts)
+
+What did we do well?
+What have we learned?
+What can we do better?
+What puzzles us?
+"""
+
+""" ----------------------------------------------------------------------
+2. In Java, describe any TWO of the consequences of marking a method
+   "abstract".
+   (2 pts)
+
+the base class must be marked abstract
+
+the method must be defined by the derived class or the derived class must
+be marked abstract
+
+the method can not be defined in the base class
+"""
